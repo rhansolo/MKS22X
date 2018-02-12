c0f3b7600a297fa3414287e394b8c89e9f7458f3
robinhanstuy <33820379+robinhanstuy@users.noreply.github.com>
2018-02-12 00:01:24 -0500

    coded add and remove queen correctly. Im going to bed zzZzzzZ...

--- 02NQueens/Driver.java
+++ 02NQueens/Driver.java
@@ -1,6 +1,9 @@
 public class Driver{
     public static void main(String[] args){
-	QueenBoard b = new QueenBoard(4);
+	QueenBoard b = new QueenBoard(6);
+	b.addQueen(2,4);
+	b.addQueen(1,1);
+	b.removeQueen(2,4);
 	System.out.println(b);
     }
 }
--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -11,7 +11,7 @@ public class QueenBoard{
 		    tmp += "Q ";
 		}
 		else{
-		    tmp += "_ ";
+		    tmp += "" + board[i][j] + " ";
 		}
 	    }
 	    tmp += "\n";
@@ -25,10 +25,52 @@ public class QueenBoard{
     public int countSolutions(){
 	return 0;
     }
-    private boolean addQueen(int r, int c){
+    public boolean addQueen(int r, int c){
+	changeBoard(r,c,1);
 	return true;
     }
-    private boolean removeQueen(int r, int c){
+    public  boolean removeQueen(int r, int c){
+	changeBoard(r,c,-1);
+	return true;
+    }
+    private boolean changeBoard(int r, int c, int increment){
+	for (int i = 0; i < board.length; i++){	    
+	    board[i][c] += increment;
+	    board[r][i] += increment;
+	}
+	int tmpr1,tmpr2,tmpr3,tmpr4;
+	int tmpc1,tmpc2,tmpc3,tmpc4;
+	tmpr1 = tmpr2 = tmpr3 = tmpr4 = r;
+	tmpc1 = tmpc2 = tmpc3 = tmpc4 = c;
+	for (int i = 0; i < board.length; i++){
+	     if (tmpr1 + 1 < board.length  && tmpc1 + 1 < board.length){
+		tmpr1 ++;
+		tmpc1 ++;
+		board[tmpr1][tmpc1] += increment;
+	    }
+	     if (tmpr2 + 1 < board.length  && tmpc2 - 1 > -1){
+		tmpr2 ++;
+		tmpc2 --;
+		board[tmpr2][tmpc2] += increment;
+	    }
+	    if (tmpr3 - 1 > -1  && tmpc3 + 1 < board.length){
+		tmpr3 --;
+		tmpc3 ++;
+		board[tmpr3][tmpc3] += increment;
+	    }
+	    if (tmpr4 -1 > -1 && tmpc4 -1 > -1){
+		tmpr4 --;
+		tmpc4 --;
+		board[tmpr4][tmpc4] += increment;
+	    }
+	    
+	}
+	if (increment < 0){
+	    board[r][c] = 0;
+	}
+	else{
+	    board [r][c] = -1;
+	}
 	return true;
     }
 }
