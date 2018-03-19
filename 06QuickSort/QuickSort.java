6261c1e4f1affa14c09c62a23abae8c415f0a931
robinhanstuy <33820379+robinhanstuy@users.noreply.github.com>
2018-03-18 23:16:59 -0400

    works?? not so sure about quickselect

--- 06QuickSort/QuickSort.java
+++ 06QuickSort/QuickSort.java
@@ -1,18 +1,37 @@
 public class QuickSort{
-    static int[] arr = new int[]{0,1,2,0,0,2,1,1,2,2,0,0,2,0,1,2,0,2,2,0,1,2,0};
+    static int[] arr = new int[]{0,6,4,2,4,7,8,4,34,67,7,3,5,8,5,3,2};
     int pivot = 0; 
     
     public static void main(String[] args){
-	sort(arr,0,arr.length-1);
+	quicksort(arr);
+	
+	System.out.println(quickselect(arr,6));
 	for (int i = 0; i < arr.length; i++){
 	    System.out.print(arr[i]);
 	}
     }
-    public static void sort(int[] arr, int low, int high){
-	helpSort(arr,low,low+1,high);
+    public static void quicksort(int[] arr){
+	quicksortHelper(arr, 0, arr.length -1);
     }
-    public static void helpSort(int[] arr, int lt, int i, int gt){
-	int v = arr[lt];
+    public static void quicksortHelper(int[] arr, int low, int high){
+	if (low == high){
+	    return;
+
+	}
+	int[] indexs = partition(arr,low,low,high);
+	if (indexs[0]!= low){
+	    quicksortHelper(arr,low,indexs[0]-1);
+	}
+	if (indexs[1] != high){
+	    quicksortHelper(arr,indexs[0]+1,high);
+	}
+    }
+    public static void sort(int[] arr, int low, int high){
+	partition(arr,low,low+1,high);
+    }
+    public static int[]  partition(int[] arr, int lt, int i, int gt){
+	int index = (int)(Math.random()*(gt-lt+1)+lt);
+	int v = arr[index];
 	while (i <= gt){
 	    if (arr[i] < v){
 		swap(arr,i,lt);
@@ -27,6 +46,25 @@ public class QuickSort{
 	    else{
 		i ++;
 	    }
+	}
+	int[] indexs = {lt,gt};
+	return indexs;
+    }
+
+    public static int quickselect(int[] arr, int k){
+	return selecthelper(arr,k,0,arr.length-1);
+    }
+    public static int selecthelper(int[] arr, int k, int low, int high){
+	int[]indexs = partition(arr, low,low ,high);
+	if(indexs[1] >= k-1&& indexs[0]<= k-1 ){
+	    return arr[indexs[0]];
+	}
+	if(indexs[0] < k-1){
+	    return selecthelper(arr,k,indexs[1]+1,high);	    
+	}
+	else{
+	    return selecthelper(arr,k,low,indexs[0]-1);
+	
 	}
     }
     public static void swap(int[] arr, int i1, int i2){
