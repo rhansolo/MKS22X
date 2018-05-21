7ee92d3cbe4a67c878973d8c8cd86f94405aaf64
robinhanstuy <33820379+robinhanstuy@users.noreply.github.com>
2018-05-21 00:29:06 -0400

    forgot another constructor

--- 13Median/RunningMedian.java
+++ 13Median/RunningMedian.java
@@ -1,16 +1,21 @@
 import java.util.*;
 public class RunningMedian{
-    private Double med;
     private MyHeap<Double> max;
     private MyHeap<Double> min;
-    
+    private double med;
  
     
     public RunningMedian(){
-	max = new MyHeap<Double>();
-	min = new MyHeap<Double>(false);
+	max = new MyHeap<Double>(false);
+	min = new MyHeap<Double>();
     }
-    
+    public RunningMedian(double[] arr){
+	min = new MyHeap<Double>();
+	max = new MyHeap<Double>(false);
+	for (int i = 0; i < arr.length; i++){
+	    add(arr[i]);
+	}
+    }
     public Double getMedian(){
 	
 	if(max.size() == 0 && min.size() == 0){
@@ -28,36 +33,42 @@ public class RunningMedian{
 	else{
 	    med = (max.peek() + min.peek()) / 2;
 	}
+
 	return med;
     }
 
     public void add(Double val){
-
-	if(max.size() == 0 && min.size() == 0){
-	    max.add(val);
- 
-	    med = val;
-	 
+	if(min.size() == 0 && max.size() == 0){
+	    min.add(val);
 	}
 	else{
-	    if(val > med){
+	    if(val < min.peek()){
 		min.add(val);
+		if(min.size() - max.size() == 2){
+		    max.add(min.remove());
+		}
 	    }
 	    else{
 		max.add(val);
-	    }
-	    
-	    
-	    if((max.size() - min.size()) >1){
-		min.add(max.remove());
-	    }
-	    else if((min.size() - max.size()) > 1){
-		max.add(min.remove());
+		if(max.size() - min.size() == 2){
+		    min.add(max.remove());
+		}
 	    }
 	}
     }
     public int size(){
 	return max.size() + min.size();
     }
-    
+    public static void main(String[] args){
+    double[] data = {1.0,3.0,5.0,4.0,2.0};
+    RunningMedian a = new RunningMedian(data);
+    System.out.println(a.getMedian()); //3
+ System.out.println(a.min);
+    System.out.println(a.max);
+    a.add(6.0);
+    System.out.println(a.getMedian()); //3.5
+    a.add(1.0);
+    a.add(1.0);
+    System.out.println(a.getMedian());//2.5
+  }
 }
