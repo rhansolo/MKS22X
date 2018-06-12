4764d5e6d5466b66cf2c154caa3503c419625f4e
robinhanstuy <33820379+robinhanstuy@users.noreply.github.com>
2018-06-11 22:15:44 -0400

    tested and should now work

--- 06QuickSort/Quick.java
+++ 06QuickSort/Quick.java
@@ -1,78 +1,124 @@
+import java.util.*;
+
 public class Quick{
-    static int[] arr = new int[]{0,6,4,2,4,7,8,4,34,67,7,3,5,8,5,3,2};
-    int pivot = 0; 
     
-    public static void main(String[] args){
-	quicksort(arr);
-	
-	System.out.println(quickselect(arr,6));
-	for (int i = 0; i < arr.length; i++){
-	    System.out.print(arr[i]);
-	}
+    public static void quicksort(int[] data){
+	sortHelper(data, 0 , data.length -1);
     }
-    public static void quicksort(int[] arr){
-	quicksortHelper(arr, 0, arr.length -1);
-    }
-    public static void quicksortHelper(int[] arr, int low, int high){
-	if (low == high){
-	    return;
+    
+    public static void sortHelper(int[] data, int start, int end){
+	if(end>start){
+	    
+	    int[] arr = selectHelper (data,start,end);
+	    
+	    if(arr[1]>=arr[0]){
+		
+		if(start!=arr[0]){
+
+		    sortHelper(data,start,arr[0]);
+		   }
+		if(end!=arr[1]){
+
+		    sortHelper(data,arr[1],end);
+		}
+	    }
 
 	}
-	int[] indexs = partition(arr,low,low,high);
-	if (indexs[0]!= low){
-	    quicksortHelper(arr,low,indexs[0]-1);
-	}
-	if (indexs[1] != high){
-	    quicksortHelper(arr,indexs[0]+1,high);
-	}
     }
-    public static void sort(int[] arr, int low, int high){
-	partition(arr,low,low+1,high);
-    }
-    public static int[]  partition(int[] arr, int lt, int i, int gt){
-	int index = (int)(Math.random()*(gt-lt+1)+lt);
-	int v = arr[index];
-	while (i <= gt){
-	    if (arr[i] < v){
-		swap(arr,i,lt);
-		lt++;
-		i++;
+    public static int quickselect(int []data, int k){
+	int start=0;
+	int end=data.length-1;
+        for(int i=0;i<data.length;i++){
+	    
+	    int arr = partition(data,start,end);
+
+	    if(arr==k){
+		return data[arr];
 	    }
-	    else if (arr[i] > v){
-		swap(arr,i,gt);
-		gt--;
-	     
+	    if(k > arr){
+	        start=arr+1;
+	    }
+	    if(k < arr){
+		end=arr-1;
+	    }
+	}
+	return -1;
+    }
+    public static int[] selectHelper ( int [] data, int start, int end){
+	int[] arrs=new int[2];
+	
+	Random rand = new Random();
+	
+	int  pivot = rand.nextInt(end-start+1)+start;
+	swap(data,start,pivot);
+
+	
+	int ind = start+1;
+	while(ind<=end){
+	     if(data[ind]==data[start]){
+		 ind++;
+        
+	     }
+	     else if(data[ind]<data[start]){
+		swap(data,ind,start);
+		start++;
+		ind++;
 	    }
 	    else{
-		i ++;
+		swap(data,ind,end);
+		end--;
 	    }
 	}
-	int[] indexs = {lt,gt};
-	return indexs;
-    }
-
-    public static int quickselect(int[] arr, int k){
-	return selecthelper(arr,k,0,arr.length-1);
-    }
-    public static int selecthelper(int[] arr, int k, int low, int high){
-	int[]indexs = partition(arr, low,low ,high);
-	if(indexs[1] >= k-1&& indexs[0]<= k-1 ){
-	    return arr[indexs[0]];
-	}
-	if(indexs[0] < k-1){
-	    return selecthelper(arr,k,indexs[1]+1,high);	    
-	}
-	else{
-	    return selecthelper(arr,k,low,indexs[0]-1);
+	arrs[0]=start;
+	arrs[1]=ind;
 	
+	return arrs;
+    }
+
+
+    public static int partition ( int [] data, int start, int end){
+	Random rand = new Random();
+	int  pivot = rand.nextInt(end-start+1)+start;
+	swap(data,start,pivot);
+	int lo=start;
+	start++;
+	int i=start;
+	while(i<end){
+	    if(data[i]==data[lo]){
+		i++;
+	    }
+	    if(data[i]<data[lo]){
+		swap(data,i,start);
+		start++;
+		i++;
+	    }
+	    if(data[i]>data[lo]){
+		swap(data,i,end);
+		end--;
+	    }
 	}
+	if(data[end]>data[lo]){
+	    swap(data,end-1,lo);
+	    end--;
+	}
+        else{
+	    swap(data,end,lo);
+	}
+
+	return end;
     }
-    public static void swap(int[] arr, int i1, int i2){
-	int tmp = arr[i1];
-	arr[i1] = arr[i2];
-	arr[i2] = tmp;
 
+    public static void swap(int[] data,int x1, int x2){
+	int tmp =data[x1];
+	data[x1]=data[x2];
+	data[x2]=tmp;
     }
 
-
+    public static void toString(int[] data){
+	String ans ="";
+	for(int i=0;i<data.length;i++){
+	    ans +=data[i]+" ";
+	}
+	System.out.println(ans);
+    }
 }
